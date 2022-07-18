7f21855f0b021afb2540cb15fb715107d122ec8a
Chercheuse_du_feu <lesyaderkach.lesya@gmail.com>
2022-07-18 12:56:34 +0400

    adjusted dots active status for my carousel

--- JS/carousel.js
+++ JS/carousel.js
@@ -1,6 +1,24 @@
 var dot = document.getElementsByClassName("dot");
 var slides = document.getElementById('slides');
 
-dot[0].onclick = function () { slides.style.transform = "translateX(0px)" };
-dot[1].onclick = function () { slides.style.transform = "translateX(-800px)" };
-dot[2].onclick = function () { slides.style.transform = "translateX(-1600px)" };
+dot[0].onclick = function () {
+    slides.style.transform = "translateX(0px)";
+    for (i = 0; i < 3; i++) {
+        dot[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+dot[1].onclick = function () {
+    slides.style.transform = "translateX(-800px)";
+    for (i = 0; i < 3; i++) {
+        dot[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+dot[2].onclick = function () {
+    slides.style.transform = "translateX(-1600px)";
+    for (i = 0; i < 3; i++) {
+        dot[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
