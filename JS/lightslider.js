e75e75707c8cf680ae2cc955d739e400dc5e1994
vikahromova <103583134+vikahromova@users.noreply.github.com>
2022-07-22 14:57:34 +0300

    lighttsliderjs

--- JS/lightslider.js
+++ JS/lightslider.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("#lightSlider").lightSlider({
-		item: 4,
+		item: $(window).width() < 900 ? 2 : 4,
 		autoWidth: false,
 		slideMove: 1, // slidemove will be 1 if loop is true
 		slideMargin: 10,
