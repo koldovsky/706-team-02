7c0e4a6ccf1863a12e78bed36e46829dc071b846
vikahromova <103583134+vikahromova@users.noreply.github.com>
2022-07-22 14:47:04 +0300

    js

---
+++ JS/lightslider.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$("#lightSlider").lightSlider({
+		item: 4,
+		autoWidth: false,
+		slideMove: 1, // slidemove will be 1 if loop is true
+		slideMargin: 10,
+		mode: "slide",
+	});
+});
