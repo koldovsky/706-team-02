91f0f81245c0ad4294206d9c2c5f727fb5c4699f
Lessia Derkach <chercheuse_du_feu@Lessias-MacBook-Air.local>
2022-07-28 11:28:06 +0400

    updated css

--- js/testimonials-carousel.js
+++ js/testimonials-carousel.js
@@ -1,6 +1,6 @@
 (function () {
 const dot = document.getElementsByClassName("testimonials__dot");
-const slides = document.getElementById('Testimonials__slides');
+const slides = document.getElementById('testimonials__slides');
 
 dot[0].onclick = function () {
     slides.style.transform = "translateX(0px)";
