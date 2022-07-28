ba70aff247c9355ad895e8620a960be5b2222fe3
Lessia Derkach <chercheuse_du_feu@Lessias-MacBook-Air.local>
2022-07-28 20:14:52 +0400

    added styles for buttons

--- js/testimonials-carousel.js
+++ js/testimonials-carousel.js
@@ -1,27 +1,34 @@
 (function () {
-const dot = document.getElementsByClassName("testimonials__dot");
-const slides = document.getElementById('testimonials__slides');
+    const dot = document.getElementsByClassName("testimonials__dot");
+    const slides = document.getElementById('testimonials__slides');
 
-dot[0].onclick = function () {
-    slides.style.transform = "translateX(0px)";
-    for (i = 0; i < 3; i++) {
-        dot[i].classList.remove("active");
+    dot[0].onclick = function () {
+        slides.style.transform = "translateX(0px)";
+        for (i = 0; i < 3; i++) {
+            dot[i].classList.remove("active");
+        }
+        this.classList.add("active");
     }
-    this.classList.add("active");
-}
-dot[1].onclick = function () {
-    slides.style.transform = "translateX(-800px)";
-    for (i = 0; i < 3; i++) {
-        dot[i].classList.remove("active");
+    dot[1].onclick = function () {
+        slides.style.transform = "translateX(-800px)";
+        for (i = 0; i < 3; i++) {
+            dot[i].classList.remove("active");
+        }
+        this.classList.add("active");
     }
-    this.classList.add("active");
-}
-dot[2].onclick = function () {
-    slides.style.transform = "translateX(-1600px)";
-    for (i = 0; i < 3; i++) {
-        dot[i].classList.remove("active");
+    dot[2].onclick = function () {
+        slides.style.transform = "translateX(-1600px)";
+        for (i = 0; i < 3; i++) {
+            dot[i].classList.remove("active");
+        }
+        this.classList.add("active");
     }
-    this.classList.add("active");
-}
+
+    const prevButton = document.querySelector('.buttons__prev');
+    prevButton.addEventListener('click', prev);
+
+    const nextButton = document.querySelector('.buttons__next');
+    nextButton.addEventListener('click', next);
 
 })();
+
