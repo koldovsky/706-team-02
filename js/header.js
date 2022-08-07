393ad8a91e72ab04ab929f81f140827df9ad2fee
Vitaly-kr <danylenko.va89@gmail.com>
2022-08-07 14:33:15 +0300

    header56

--- js/header.js
+++ js/header.js
@@ -1,4 +1,13 @@
 let burgerMenuBlock = document.querySelector('.burger')
 burgerMenuBlock.addEventListener('click', (elem)  => {
-    burgerMenuBlock.classList.toggle('active');
+    burgerMenuBlock.classList.toggle('active');    
+    let menu=document.querySelector('.burger__menu')
+    menu.classList.toggle('burger__menu-active');
+    })
+
+let menuLink = document.querySelector('.menu__link')
+menuLink.addEventListener('click', (elem)  => {
+    elem.preventDefault()    
+    let menu=document.querySelector('.burger__menu')
+    menu.classList.toggle('burger__menu-active');
 })
